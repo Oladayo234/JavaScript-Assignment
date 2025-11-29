cea8f768f7232a849047c99e4b4c9f9998cf3ff3
Oladayo234 <oladayoolubunmi20@gmail.com>
2025-11-29 06:33:31 +0100

    welcome to JavaScript

---
+++ class work/javascript_test_file.js
@@ -0,0 +1,56 @@
+//let name = "dayo";
+//console.log (name)
+//    var handler = "omo ologo"
+//    console.log (handler)
+
+
+//var one = 5;
+//var two = 29;
+//var three = 15;
+//var four 
+//
+//    one = 
+//    two =
+//
+//console.log(three contains the value of two)
+
+
+
+let firstNumber = 20;
+let secondNumber = 30;
+
+let sum = firstNumber + secondNumber;
+
+console.log(sum)
+
+
+
+let numberOne = 106;
+
+division = numberOne % 10;
+
+total = numberOne + division;
+
+console.log(total)
+
+
+
+let userInputOne = 2;
+let userInputTwo = 6;
+
+let multiplication = userInputOne ** 6;
+
+console.log(multiplication) 
+
+
+
+let value = 20;
+let firstValue;
+let secondValue;
+let thirdValue;
+
+firstValue = value + 10;
+secondValue =
+
+
+
